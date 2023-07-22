9b7639d8723ef4cb460001cd4d46478f631a7c2b
magnitude12 <113277446+magnitude12@users.noreply.github.com>
2023-07-22 12:10:18 +0545

    Create distancecheck.ino

---
+++ distancecheck.ino
@@ -0,0 +1,29 @@
+const int trigPin = 5;
+const int echoPin = 18;
+
+#define SOUND_SPEED 0.033
+
+long duration;
+float distanceCm;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * SOUND_SPEED/2;
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+
+  
+  delay(1000);
+}
